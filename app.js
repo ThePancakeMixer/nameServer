19d6d77ff47b7b277a74e9a7e0223590bc7453d5
Nikesh <nikeshp96@gmail.com>
2020-06-02 23:14:29 -0700

    added gettop5 function, changed up how to deliver json to front end

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const sess = {
     secret : 'zPLaW.....e',
     resave: false,
     saveUninitialized: false,
-    cookie: { maxAge: 8*60*60*1000}
+    cookie: { maxAge: 1*60*1000}
 };
 
 app.use(session(sess))
@@ -40,30 +40,92 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(buildPath,'index.html'));
   });
 
-app.post('/addName',(req,res) => 
-    {
-        if(req.session.name){
-            res.send('You can vote again in ' + Math.floor(req.session.cookie.maxAge / 60000) + ' minutes')
-            return;
-        }
-        req.session.name = "touched"
-        let name = req.body.name
-        console.log(req.session.id);
+app.get('/getTopFive', function(req,res){
 
-        let request = new Request('AddName',function(error){
-            if(error){
-                console.log(error)
+    let request = new Request(
+        'SELECT TOP 5 * FROM NameTable ORDER BY NameCnt DESC',
+        (err, rowCount) => {
+            if (err) {
+                console.error(err.message);
+            } else {
+                console.log(`${rowCount} row(s) returned`);
             }
+        }
+    )
+    let rowsText = [];
+    request.on("row", columns => {
+        let rowText = ""
+        columns.forEach(column => 
+        {
+            rowText = rowText +  column.value + " "
         })
-        request.addParameter('Name',TYPES.VarChar, name)
-        request.addOutputParameter('NameCount',TYPES.Int)
+        rowsText.push(rowText)
+    });
+    request.on('requestCompleted', function () { 
+        console.log(rowsText)
+        res.send(rowsText)
+    });
 
-        request.on('returnValue', function(paramaterName, value,metadta){
-            console.log(paramaterName + ' = ' + value)
-            res.send(value + " people said fuck " + name)
+    connection.execSql(request)
+})
+
+function isEmptyOrSpaces(str){
+    return str === null || str.match(/^ *$/) !== null;
+}
+
+let getTotal = function(req,res){
+    let name = req.body.name
+    if(isEmptyOrSpaces(name) || name.length>25){
+        res.send({
+            one: 'Name must be between 1 and 25 characters long'
         })
-        connection.callProcedure(request)
+        return
     }
-);
+    
+    let request = new Request('GetTotal',function(error){
+        if(error){
+            console.log(error)
+        }
+    })
+    request.addParameter('Name',TYPES.VarChar, name)
+    request.addOutputParameter('NameCount',TYPES.Int)
+    request.on('returnValue', function(paramaterName, value,metadta){
+        console.log(paramaterName + ' = ' + value)
+        let ret = {
+            one: (value || 0) + " people said fuck " + name,
+            two: 'Vote Not Counted',
+            three: 'You can vote again in ' + Math.floor(req.session.cookie.maxAge / 600) + ' seconds'
+        }
+        res.send(ret)
+    })
+    connection.callProcedure(request)
+}
+app.post('/addName',(req,res) => 
+{
+    if(req.session.name){
+        getTotal(req,res)
+        return;
+    }
+    req.session.name = "touched"
+    let name = req.body.name
+    console.log(req.session.id);
+
+    let request = new Request('AddName',function(error){
+        if(error){
+            console.log(error)
+        }
+    })
+    request.addParameter('Name',TYPES.VarChar, name)
+    request.addOutputParameter('NameCount',TYPES.Int)
+
+    request.on('returnValue', function(paramaterName, value,metadta){
+        console.log(paramaterName + ' = ' + value)
+        let ret = {
+            one: value + " people said fuck " + name,
+        }
+        res.send(ret)
+    })
+    connection.callProcedure(request)
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
